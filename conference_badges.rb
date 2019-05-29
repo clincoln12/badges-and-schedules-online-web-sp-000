b53f06e3cee85b87724a5cb18cb77c6b65ce3437
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-29 23:27:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,9 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speakers)
+  rooms = []
+  speakers.each do |x|
+    rooms << "Hello, #{x}! You'll be assigned to room #{x.index + 1}"
+  end
+  rooms
 end
