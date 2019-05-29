5cf0a201007e839958f54c912e4e679209cf5952
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-29 22:41:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr = []
+  array.each { |x| arr << badge_maker(x) }
+  arr
+end
+
+def assign_rooms(speaker)
+  
+end
