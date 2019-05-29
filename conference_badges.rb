a17a743c80dd01038f2e495c238d1a299325efd4
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-29 23:43:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,8 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  attendees.each do |a|
+    puts batch_badge_creator(a)
+    puts assign_rooms(a)
+  end
 end
